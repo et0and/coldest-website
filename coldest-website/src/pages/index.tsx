6069c769ecd6bc648a2f3728448e61fc41ccf436
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-05-15 10:19:32 +0000

    Removed image

--- coldest-website/src/pages/index.tsx
+++ coldest-website/src/pages/index.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import messageImage from '../public/message.png';
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <Image src={messageImage} alt="Message" />
+      
     </div>
   );
 }
