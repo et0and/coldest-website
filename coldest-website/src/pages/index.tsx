df1a022e99de024c52097040171a088eaee46b8c
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-05-15 11:29:53 +0000

    String error?

--- coldest-website/src/pages/index.tsx
+++ coldest-website/src/pages/index.tsx
@@ -1,31 +1,34 @@
 import Head from 'next/head'
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <Head>
         <title>Coldest website</title>
         <meta property="og:title" content="Coldest website" key="title" />
-        <meta property="twitter:title" content="Coldest website""/>
+        <meta property="twitter:title" content="Coldest website"/>
         <meta name="theme-color" content="#000000" />
         <meta
           property="description"
-          content="Open the door"
+          content="A door awaits to be opened"
         />
         <meta
           property="og:description"
-          content="Open the door"
+          content="A door awaits to be opened"
         />
         <meta
           property="twitter:description"
-          content="Open the door"
+          content="A door awaits to be opened"
         />
         <meta property="og:url" content="https://work.tom.so/coldest-website" />
         <meta property="og:image" content="og.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:image" content="og.png"/>
       </Head>
-    <div className="flex items-center justify-center">      
+      
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
